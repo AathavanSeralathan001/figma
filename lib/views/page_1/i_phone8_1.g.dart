def8dbaa15af0a5dbdcddbcec191fcaaa664a5b9
Parabeac-Nest <admin@parabeac.com>
2022-08-01 02:40:14 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/i_phone8_1.g.dart
@@ -0,0 +1,285 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class IPhone81 extends StatefulWidget {
+  const IPhone81({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _IPhone81 createState() => _IPhone81();
+}
+
+class _IPhone81 extends State<IPhone81> {
+  _IPhone81();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Stack(children: [
+        Positioned(
+          left: 24.0,
+          width: 409.762,
+          top: 250.0,
+          height: 410.511,
+          child: Container(
+            height: 410.5114440917969,
+            width: 409.7617492675781,
+            decoration: BoxDecoration(
+              color: Color(0xd131c0dd),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          width: 409.762,
+          top: 712.0,
+          height: 410.511,
+          child: Container(
+            height: 410.5114440917969,
+            width: 409.7617492675781,
+            decoration: BoxDecoration(),
+          ),
+        ),
+        Positioned(
+          left: 410.0,
+          width: 409.762,
+          top: 0,
+          height: 410.511,
+          child: Container(
+            height: 410.5114440917969,
+            width: 409.7617492675781,
+            decoration: BoxDecoration(
+              color: Color(0xffc2f5ff),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 379.0,
+          width: 168.0,
+          top: 517.0,
+          height: 53.0,
+          child: Container(
+              height: 53.0,
+              width: 168.0,
+              child: AutoSizeText(
+                'Skewees',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Color(0xff056ca6),
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 265.0,
+          width: 362.0,
+          top: 604.0,
+          height: 266.0,
+          child: Container(
+            height: 266.0,
+            width: 362.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.0, -1.0),
+                end: Alignment(0.0, 0.9999999999999998),
+                colors: <Color>[
+                  Color(0xff69d4e9),
+                  Color(0x75ade8f4),
+                ],
+                stops: [
+                  0,
+                  1,
+                ],
+                tileMode: TileMode.clamp,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(39)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 307.0,
+          width: 102.0,
+          top: 774.0,
+          height: 15.0,
+          child: Container(
+              height: 15.0,
+              width: 102.0,
+              child: AutoSizeText(
+                'Forgot Password?',
+                style: TextStyle(
+                  fontFamily: 'Poly',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 383.0,
+          width: 119.0,
+          top: 822.0,
+          height: 39.0,
+          child: Container(
+            height: 39.0,
+            width: 119.0,
+            decoration: BoxDecoration(
+              color: Color(0xffade8f4),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 406.0,
+          width: 168.0,
+          top: 822.0,
+          height: 53.0,
+          child: Container(
+              height: 53.0,
+              width: 168.0,
+              child: AutoSizeText(
+                'LOGIN',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 302.0,
+          width: 288.0,
+          top: 638.0,
+          height: 47.0,
+          child: Container(
+            height: 47.0,
+            width: 288.0,
+            decoration: BoxDecoration(
+              color: Color(0xfff2f8f4),
+              border: Border.all(
+                color: Color(0xff0c88fa),
+                width: 0.5,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 331.0,
+          width: 104.0,
+          top: 646.0,
+          height: 30.0,
+          child: Container(
+              height: 30.0,
+              width: 104.0,
+              child: AutoSizeText(
+                'Username',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 302.0,
+          width: 292.0,
+          top: 718.0,
+          height: 45.0,
+          child: Container(
+            height: 45.0,
+            width: 292.0,
+            decoration: BoxDecoration(
+              color: Color(0xfff2f8f4),
+              border: Border.all(
+                color: Color(0xff0c88fa),
+                width: 0.5,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 331.0,
+          width: 97.0,
+          top: 726.0,
+          height: 30.0,
+          child: Container(
+              height: 30.0,
+              width: 97.0,
+              child: AutoSizeText(
+                'Password',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 547.0,
+          width: 31.0,
+          top: 646.0,
+          height: 35.0,
+          child: Image.asset(
+            'assets/images/kisspngcomputericonsuserprofile5afd955d62a2c81.png',
+            package: 'sample_project',
+            height: 35.0,
+            width: 31.0,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 549.0,
+          width: 27.821,
+          top: 723.0,
+          height: 35.0,
+          child: Image.asset(
+            'assets/images/nicepngpasswordiconpng20731151.png',
+            package: 'sample_project',
+            height: 35.0,
+            width: 27.820510864257812,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 387.983,
+          width: 132.034,
+          top: 555.5,
+          height: 4.0,
+          child: SvgPicture.asset(
+            'assets/images/line1.svg',
+            package: 'sample_project',
+            height: 4.0,
+            width: 132.03408813476562,
+            fit: BoxFit.none,
+          ),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
